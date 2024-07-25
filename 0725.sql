6e1877af176e0f11541e85cee2774f6926eb8735
offpython <offpython@gmail.com>
2024-07-25 15:47:45 +0900

    7월 25일 MySQL 수업 소스

--- 0725.sql
+++ 0725.sql
@@ -22,4 +22,29 @@ VALUES(7999, 'TOM', 'SALESMAN', 7782, curdate(), 2000, 2000, 10);
 rollback TO aaa;
 
 
+CREATE TABLE  emp20  AS 
+SELECT empno, ename, sal
+FROM emp
+WHERE deptno = 20;
 
+ALTER TABLE emp20
+ADD age TINYINT AFTER ename;
+
+ALTER TABLE emp20 
+DROP COLUMN sal;
+
+SELECT * FROM emp20;
+
+ALTER TABLE emp20
+MODIFY ename VARCHAR(20);
+
+CREATE TABLE Jusorok
+(
+	bunho SMALLINT,
+    gender CHAR(6) DEFAULT 'MALE'
+);
+
+INSERT INTO Jusorok VALUE(1, 'FEMALE');
+INSERT INTO Jusorok VALUE(2, DEFAULT);
+
+SELECT * FROM Jusorok
